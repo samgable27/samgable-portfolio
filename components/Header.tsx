f3f62e36e393c2d3fa8225c9493fc5dc60dd5e79
Sam <samarooniii@yahoo.com>
2023-02-23 23:22:23 -0500

    header complete

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,22 @@ type Props = {};
 export default function Header({}: Props) {
   return (
     <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
-      <motion.div className="flex flex-row items-center">
+      <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center"
+      >
         {/* Social Icons */}
         <SocialIcon
           url="https://github.com/samgable27"
@@ -26,7 +41,22 @@ export default function Header({}: Props) {
         />
       </motion.div>
 
-      <div className="flex flex-row items-center text-gray-300 cursor-pointer">
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center text-gray-300 cursor-pointer"
+      >
         <SocialIcon
           className="cursor-pointer"
           network="email"
@@ -36,7 +66,7 @@ export default function Header({}: Props) {
         <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
           Get In Touch
         </p>
-      </div>
+      </motion.div>
     </header>
   );
 }
